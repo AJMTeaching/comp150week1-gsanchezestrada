85aa4e87d9bf4a36dadb4f0b52cb08184c25b7a4
gsanchezestrada <gsanchezestrada@luc.edu>
2025-02-12 22:58:18 +0000

    Line 121 Passed: Updated sorting of order

--- main.py
+++ main.py
@@ -90,6 +90,11 @@ def merge_lists(list1: list, list2: list) -> list:
 
     while i < len(list1) and j < len(list2):
         if list1[i] < list2[j]:
+            merged.append(list1[i])
+            i += 1 
+        else:
+            merged.append(list2[j])
+            j += 1
             
     return merged 
 
